673eeb660923eb2a2625d30d1d6c36ace1c9163b
Parwat Kunwar <parwatkunwar08@gmail.com>
2024-10-15 23:55:35 -0300

    update on fetch jobs func

--- python-script/tracker.py
+++ python-script/tracker.py
@@ -19,16 +19,22 @@ SMTP_PORT = 587
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
 def fetch_job_postings(urls):
-
-
-
-  for url in urls:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
     jobs = []
-    for job_element in soup.find_all('div', id='jobs'):
-      title
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
+        # Assuming job postings are contained within a div with id='jobs'
+        job_elements = soup.find_all('div', class_='job')  # Adjust this based on actual HTML structure
+        for job_element in job_elements:
+            title = job_element.find('h2').text.strip()
+            location = job_element.find('div', class_='location').text.strip() if job_element.find('div', class_='location') else 'No location'
+            link = job_element.find('a')['href']
 
-def send_email(new_jobs):
+            jobs.append({
+                'title': title,
+                'location': location,
+                'link': link
+            })
+
+    return jobs
